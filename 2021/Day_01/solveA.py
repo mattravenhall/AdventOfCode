14758b82c2f5a5ca07d282f071cb6b164feddb18
mattravenhall <mattravenhall@users.noreply.github.com>
2021-12-01 22:30:13 +0000

    Adding solutions for 2021 Day_01 (Part B)

--- 2021/Day_01/solveA.py
+++ 2021/Day_01/solveA.py
@@ -2,4 +2,4 @@
 
 measurements = list(map(lambda x: int(x.rstrip()), open('input.txt').readlines()))
 
-print(sum([x > measurements[i-1] for i, x in enumerate(measurements) if i > 0]))
+print(sum([measurements > measurements[i-1] for i, x in enumerate(measurements) if i > 0]))
