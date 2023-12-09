6f38b7444b1a4f81681785179d64cf3bf5b7d95f
mattravenhall <mattravenhall@users.noreply.github.com>
2023-12-09 07:56:11 +0000

    Adding solutions for 2023 Day_09

---
+++ 2023/Day_09/solveB.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+result = 0
+
+def next_in_sequence(sequence: list[int]) -> int:
+    differences = [sequence[i] - sequence[i-1] for i in range(1, len(sequence))]
+
+    if len(set(differences)) == 1:
+        return sequence[0] - differences[0]
+    else:
+        return sequence[0] - next_in_sequence(differences)
+
+
+for line in open("input.txt").readlines():
+    sequence = list(map(int, line.strip().split()))
+
+    next_value = next_in_sequence(sequence)
+    result += next_value
+
+print(result)
