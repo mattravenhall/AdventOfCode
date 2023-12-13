4a30aa4d239abd9903c8d7264dc1672e68a10421
mattravenhall <mattravenhall@users.noreply.github.com>
2023-12-13 23:39:21 +0000

    Adding solutions for 2023 Day_13

---
+++ 2023/Day_13/solveA.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+
+
+def process_pattern(pattern: list[list[str]]) -> int:
+    def _find_symmetry(pattern: list[list[str]]) -> Optional[int]:
+        for i in range(len(pattern)-1):
+            location = j = i+1
+            while 0 <= i <= len(pattern)-2 and 1 <= j <= len(pattern)-1:
+                if pattern[i] == pattern[j]:
+                    i -= 1
+                    j += 1
+                else:
+                    break
+            else:
+                return location
+
+    # Consider horizontal reflections
+    row_score = _find_symmetry(pattern)
+    if row_score is not None:
+        print(f"Pattern found on row {row_score}")
+        return row_score * 100
+
+    # Consider vertical reflections
+    pattern = list(map("".join, zip(*reversed(pattern))))
+    col_score = _find_symmetry(pattern)
+    if col_score is not None:
+        print(f"Pattern found on col {col_score}")
+        return col_score
+    
+    return 0
+
+
+total = 0
+pattern = []
+for line in open("input.txt").readlines()+['']:
+    line = line.strip()
+    if line == '':
+        # Process pattern and refresh
+        total += process_pattern(pattern)
+        pattern = []
+    else:
+        pattern.append(line)
+
+print(total)
