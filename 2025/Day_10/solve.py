eb1cb9a3057364d2c102c3a47ee8ed4f0150fb78
mattravenhall <mattravenhall@users.noreply.github.com>
2025-12-14 21:36:12 +0000

    Adding Python solution for 2025 Day_10 Part Two

--- 2025/Day_10/solve.py
+++ 2025/Day_10/solve.py
@@ -5,6 +5,9 @@ from functools import reduce
 from itertools import combinations
 from operator import ior
 
+import numpy as np
+from scipy.optimize import Bounds, LinearConstraint, milp
+
 
 def build_button(group: str, width: int) -> str:
     group_nums = set(map(int, group.split(',')))
@@ -20,12 +23,13 @@ def press_buttons(states: list[tuple[int]]) -> list[int]:
     total = list(map(clean, (map(sum, zip(*states)))))
     return total
 
+
 def solve(path: str) -> int:
     machines = []
     re_groups = r'^\[(.*)\] (\(.*\))+ {(.*)}\\n$'
     part_one = 0
+    part_two = 0
 
-    # Part One - minimum button press
     for line in open(path).readlines():
         min_size = None
         goal = list(map(int, re.findall(r"\[(.*)\]", line)[0].replace('.', '0').replace('#', '1')))
@@ -37,7 +41,7 @@ def solve(path: str) -> int:
         ]
         joltages = list(map(int, re.findall(r"{(.*)}", line)[0].split(',')))
 
-        # Bruteforce minimal combinations needed to achieve [1] * width
+        # Part One: Bruteforce minimal combinations needed to achieve [1] * width
         for group_size in range(1, width+1):
             for subset in combinations(buttons, group_size):
                 outcome = press_buttons([x for x in subset])
@@ -49,14 +53,20 @@ def solve(path: str) -> int:
 
             if min_size is not None:
                 break
-
-        if min_size is None:
-            breakpoint()
         part_one += min_size
 
-    return part_one
+        # Part Two: Minimal presses for given joltage
+        result = milp(
+            [1] * len(buttons),
+            constraints=LinearConstraint(np.array(buttons).T, joltages, joltages),  # Joltage to reach
+            bounds=Bounds(lb=0, ub=np.inf), # Prevent negative button presses
+            integrality=1   # Integers only
+        ).fun
+        part_two += result
+
+    return part_one, int(part_two)
 
 
-assert solve("test.txt") == 7
+assert solve("test.txt") == (7, 33)
 
 print(solve("input.txt"))
